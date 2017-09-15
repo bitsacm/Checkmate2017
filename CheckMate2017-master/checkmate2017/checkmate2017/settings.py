b14701e84a84dedd683e00d87374596e925ac476
priyankjraj <f2016615@pilani.bits-pilani.ac.in>
2017-09-16 02:25:07 +0530

    Update settings.py

--- CheckMate2017-master/checkmate2017/checkmate2017/settings.py
+++ CheckMate2017-master/checkmate2017/checkmate2017/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'checkmate2017.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'checkmate',
-        'USER': 'checkmateuser',
-        'PASSWORD': 'checkmate',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 # Password validation
